827017625fad5b341591c436330ab7220039d7fd
Brayant <70989902bpn@gmail.com>
2023-07-07 23:05:29 -0500

    Agregamos la funcionalidad al modelo Origin de asignar los cuadrantes a sus nodos dependientes

--- Route_Maker/RMapp/functions/assignment.py
+++ Route_Maker/RMapp/functions/assignment.py
@@ -1,35 +1,4 @@
 
-
-
-origin = {
-    'lon':0.0,
-    'lat':0.0
-}
-
-nodo_A = {
-    'lon':1.12,
-    'lat':1.64
-}
-
-nodo_B = {
-    'lon':-3.51,
-    'lat':2.37
-}
-
-nodo_C = {
-    'lon':-2.49,
-    'lat':-1.38
-}
-
-nodo_D = {
-    'lon':3.17,
-    'lat':-1.6
-}
-
-ponits = [nodo_A,nodo_B,nodo_C,nodo_D]
-
-Cuadrantes = {}
-
 equals = {
         (True,True):'I',
         (False,True):'II',
@@ -37,9 +6,7 @@ equals = {
         (True,False):'IV'
     }
 
-for i in ponits:
-    lon = origin['lon'] < i['lon']
-    lat = origin['lat'] < i['lat']
-    Cuadrantes[tuple(i.items())] = equals[(lon,lat)]
-
-print(Cuadrantes)
+def compare(origin,nodo):
+    lon = origin.lon < nodo.lon
+    lat = origin.lat < nodo.lat
+    return equals[(lon,lat)]
--- Route_Maker/RMapp/models.py
+++ Route_Maker/RMapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from functions.assignment import compare
 
 # Create your models here.
 class Origin(models.Model):
@@ -9,12 +10,21 @@ class Origin(models.Model):
     def __str__(self) -> str:
         return self.name
     
+    def assign_quadrant(self):
+        nodos = self.relational_nodos.all()
+        nodos_list = []
+        for nodo in nodos:
+            quadrant = compare(origin=self,nodo=nodo)
+            nodo.quadrant = quadrant
+            nodos_list.append((nodo,nodo.quadrant))
+        return nodos_list
+    
 class Nodo(models.Model):
     name = models.CharField(max_length=200)
     lat = models.DecimalField(max_digits=9, decimal_places=6)
     lon = models.DecimalField(max_digits=9, decimal_places=6)
-    origin = models.ForeignKey(Origin,on_delete=models.CASCADE)
-    cuadrante = models.CharField(max_length=2)
+    origin = models.ForeignKey(Origin,on_delete=models.CASCADE, related_name='relational_nodos')
+    quadrant = models.CharField(max_length=2)
 
     def __str__(self) -> str:
         return self.name
