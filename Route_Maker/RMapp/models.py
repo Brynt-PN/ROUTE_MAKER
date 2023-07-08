972adeb4c4a47d5b1976ae660fe2a6fca2f28c5f
Brayant <70989902bpn@gmail.com>
2023-07-07 21:06:52 -0500

    Creamos los modelos (NODOS, ORIGEN y RUTAS) para almacenar y calcular

--- Route_Maker/RMapp/models.py
+++ Route_Maker/RMapp/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Nodo(models.Model):
+    name = models.CharField(max_length=200)
+    lat = models.DecimalField(max_digits=9, decimal_places=6)
+    lon = models.DecimalField(max_digits=9, decimal_places=6)
+    cuadrante = models.CharField(max_length=2)
+
+    def __str__(self) -> str:
+        return self.name
+    
+class Origin(models.Model):
+    name = models.CharField(max_length=200)
+    lat = models.DecimalField(max_digits=9, decimal_places=6)
+    lon = models.DecimalField(max_digits=9, decimal_places=6)
+    
+    def __str__(self) -> str:
+        return self.name
+
+class Route(models.Model):
+    path = models.JSONField()
+
+    def __str__(self) -> str:
+        return self.path
