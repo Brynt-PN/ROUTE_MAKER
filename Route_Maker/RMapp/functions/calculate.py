c4d78e40e77b1bc1d36dfb58099ae9affad68236
Brayant <70989902bpn@gmail.com>
2023-07-08 01:49:57 -0500

    Gardamos los cmabios

---
+++ Route_Maker/RMapp/functions/calculate.py
@@ -0,0 +1,35 @@
+
+Origin = {
+    'lon' : 0.0,
+    'lat' : 0.0
+}
+
+Nodo_A = {
+    'lon' : 0.0,
+    'lat' : 0.0
+}
+
+Nodo_B = {
+    'lon' : 0.0,
+    'lat' : 0.0
+}
+
+Nodo_C = {
+    'lon' : 0.0,
+    'lat' : 0.0
+}
+
+Nodo_D = {
+    'lon' : 0.0,
+    'lat' : 0.0
+}
+
+Nodo_E = {
+    'lon' : 0.0,
+    'lat' : 0.0
+}
+
+Nodo_F = {
+    'lon' : 0.0,
+    'lat' : 0.0
+}
