aa2a5147adea0ba61e8b80e161f5a361e0fda894
Brayant <70989902bpn@gmail.com>
2023-07-08 23:57:23 -0500

    Ageramos primera parte de la funcion

--- Route_Maker/RMapp/functions/assignment.py
+++ Route_Maker/RMapp/functions/assignment.py
@@ -10,3 +10,4 @@ def compare(origin,nodo):
     lon = origin.lon < nodo.lon
     lat = origin.lat < nodo.lat
     return equals[(lon,lat)]
+
--- Route_Maker/RMapp/functions/calculate.py
+++ Route_Maker/RMapp/functions/calculate.py
@@ -1,4 +1,6 @@
 
+from math import sqrt
+
 Origin = {
     'lon' : 0.0,
     'lat' : 0.0
@@ -33,3 +35,39 @@ Nodo_F = {
     'lon' : 4.4,
     'lat' : 4.09
 }
+
+nodos = [Nodo_A,Nodo_B,Nodo_C,Nodo_D,Nodo_E,Nodo_F]
+
+def get_distance(origin, nodo):
+    distance = sqrt((nodo['lon'] - origin['lon'])**2 + (nodo['lat'] - origin['lat'])**2)
+    return (nodo,distance)
+
+def compare_distance(dis1,dis2):
+    return dis1<dis2
+
+distances = [get_distance(Origin,nodo)for nodo in nodos]
+#Aqui utilizamos una lambda para acceder al segundo elemento de la tupla
+# que contiene la distancia
+ordered_distance = sorted(distances, key = lambda x: x[1])
+
+ordered_nodes_distance = ordered_distance
+
+route = []
+
+i = 0
+
+for nodo_distance in ordered_nodes_distance:
+    if route is None:
+        route = route + nodo_distance[0]
+        i += 1
+    else:        
+        distance = get_distance(nodo_distance[0],ordered_nodes_distance[(i+1)][0])
+        assignmet_route = compare_distance(distance[1],nodo_distance[1])
+
+
+
+
+
+print(ordered_distance)
+
+
