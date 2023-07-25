e3ede47bb464671b64d50d5d2d4fb714398b8095
Brayant <70989902bpn@gmail.com>
2023-07-25 02:58:58 -0500

    Construimos el index y el formulario con html y Boostrap para una unica vista que generara las rutas y agregamos enlases a las RRSS

--- Route_Maker/RMapp/admin.py
+++ Route_Maker/RMapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Origin, Nodo, Route
 # Register your models here.
+
+admin.site.register(Origin)
+admin.site.register(Nodo)
+admin.site.register(Route)
